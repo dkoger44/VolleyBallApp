82c668fe3af6ae066372bfa539a14f8296df70cf
dkoger44 <dbkr222@gmail.com>
2018-04-13 09:28:58 -0400

    SpinnersAdded

--- app/src/main/java/com/example/android/volleyballapp/AddTeamActivity.kt
+++ app/src/main/java/com/example/android/volleyballapp/AddTeamActivity.kt
@@ -2,20 +2,22 @@ package com.example.android.volleyballapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 
 class AddTeamActivity : AppCompatActivity() {
-
+    var teamTypes = arrayOf("Varsity", "Junior Varsity", "Freshmen", "Club")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_team)
 
+        var teamTypes = arrayOf("Varsity", "Junior Varsity", "Freshmen", "Club")
+
         val context = this
         val addTeamBtn = findViewById<Button>(R.id.addTeamButton) as Button
         val teamNameView = findViewById<EditText>(R.id.teamNameTextBox) as EditText
-        val teamTypeView = findViewById<EditText>(R.id.teamTypeTextBox) as EditText
+        val teamTypeSpinner = findViewById<Spinner>(R.id.teamTypeSpinner) as Spinner
+        val teamSpinnerAdapter = ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item, teamTypes)
+
         addTeamBtn.setOnClickListener({
             if(teamNameView.text.toString().length > 0 &&
                     teamTypeView.text.toString().length > 0){
